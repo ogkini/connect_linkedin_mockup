4a6d444b22a75e5238ca8e2fae5d409cbb4a45f9
Orest Gkini <orestijuve@gmail.com>
2018-08-03 11:52:22 +0300

    Initial spring boot application.

---
+++ src/main/java/com/ted/TedApplication.java
@@ -0,0 +1,12 @@
+package com.ted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedApplication.class, args);
+	}
+}
