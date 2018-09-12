2d440bb0951deaa4c7562ff70bea74d18e765d8f
LSmyrnaios <lsmyrnaios@gmail.com>
2018-09-12 05:01:40 +0300

    - Allow the 'picture' to be NULL in the DataBase and change the "Users"-column-order. - Add root-user creation in the DataBase. - Add "SpringFramework-version" in "build.gradle".

--- src/main/sql/teddb.sql
+++ src/main/sql/teddb.sql
@@ -5,19 +5,22 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema teddb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `teddb` ;
-
--- -----------------------------------------------------
--- Schema teddb
--- -----------------------------------------------------
+#DROP SCHEMA IF EXISTS `teddb` ;
 CREATE SCHEMA IF NOT EXISTS `teddb` DEFAULT CHARACTER SET utf8 ;
 USE `teddb` ;
 
 -- -----------------------------------------------------
+-- User with privileges
+-- -----------------------------------------------------
+#DROP USER IF EXISTS ‘ted_user’@‘localhost’;
+CREATE USER IF NOT EXISTS 'ted_user'@'localhost' IDENTIFIED BY 'ted';
+GRANT ALL PRIVILEGES ON * . * TO 'ted_user'@'localhost';
+FLUSH PRIVILEGES;
+
+-- -----------------------------------------------------
 -- Table `teddb`.`Roles`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `teddb`.`Roles` ;
-
+#DROP TABLE IF EXISTS `teddb`.`Roles` ;
 CREATE TABLE IF NOT EXISTS `teddb`.`Roles` (
   `role_id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NOT NULL,
@@ -30,16 +33,15 @@ INSERT INTO Roles (name) VALUES ('ROLE_USER');
 -- -----------------------------------------------------
 -- Table `teddb`.`Users`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `teddb`.`Users` ;
-
+#DROP TABLE IF EXISTS `teddb`.`Users` ;
 CREATE TABLE IF NOT EXISTS `teddb`.`Users` (
   `user_id` BIGINT NOT NULL AUTO_INCREMENT,
   `firstname` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
   `email` VARCHAR(60) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
+  `picture` VARCHAR(45) NULL,
   `role_id` BIGINT NOT NULL,
-  `picture` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`),
   INDEX `fk_Users_1_idx` (`role_id` ASC),
   CONSTRAINT `fk_Users_1`
@@ -49,8 +51,8 @@ CREATE TABLE IF NOT EXISTS `teddb`.`Users` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-INSERT INTO Users (firstname, lastname, email, password, role_id, picture)
-VALUES ('admin', 'admin', 'admin@mail.com', '$2a$10$9kuCCkLnpqz2WFt2ycj7Nux3T5PhYBLuGBznW0PNdaA9VRBqgEJgS', 1, 'generic.png');
+INSERT INTO Users (firstname, lastname, email, password, picture, role_id)
+VALUES ('admin', 'admin', 'admin@mail.com', '$2a$10$9kuCCkLnpqz2WFt2ycj7Nux3T5PhYBLuGBznW0PNdaA9VRBqgEJgS', 'generic.png', 1);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
