9e0cde4d68dd798559a296c8f72f4ccba9cda08a
LSmyrnaios <lsmyrnaios@gmail.com>
2018-09-22 21:35:55 +0300

    - Navigate users and admin to their "homePage" instead of the general "welcomePage", when clicking the "Home-button". - Add the ability to "fix potential vulnerabilities" in the "AppInstaller.sh"-script.

--- scripts/AppInstaller.sh
+++ scripts/AppInstaller.sh
@@ -4,4 +4,5 @@ cd ../frontend
 npm install
 npm install @angular/cli@1.7.4	# We use Angular5
 npm install --save jwt-decode
+npm audit --force fix # Fix potential vulnerabilities.
 # Add more libraries here, if needed.
