bd9279b9d33b70123c149d5490ba29868606d333
LSmyrnaios <lsmyrnaios@gmail.com>
2018-09-25 01:30:06 +0300

    Resolve non-merged DataBase.

--- src/main/sql/teddb.sql
+++ src/main/sql/teddb.sql
@@ -5,13 +5,17 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema teddb
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `teddb` ;
+#DROP SCHEMA IF EXISTS `teddb` ;
+CREATE SCHEMA IF NOT EXISTS `teddb` DEFAULT CHARACTER SET utf8 ;
+USE `teddb`;
 
 -- -----------------------------------------------------
--- Schema teddb
+-- SUser with privileges
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `teddb` DEFAULT CHARACTER SET utf8 ;
-USE `teddb` ;
+#DROP USER IF EXISTS ‘ted_user’@‘localhost’;
+CREATE USER IF NOT EXISTS 'ted_user'@'localhost' IDENTIFIED BY 'ted';
+GRANT ALL PRIVILEGES ON * . * TO 'ted_user'@'localhost';
+FLUSH PRIVILEGES;
 
 -- -----------------------------------------------------
 -- Table `teddb`.`Roles`
@@ -38,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `teddb`.`Users` (
   `lastname` VARCHAR(45) NOT NULL,
   `email` VARCHAR(60) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
+  `picture` VARCHAR(200) NULL,
   `role_id` BIGINT NOT NULL,
-  `picture` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`),
   INDEX `fk_Users_1_idx` (`role_id` ASC),
   CONSTRAINT `fk_Users_1`
@@ -50,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `teddb`.`Users` (
 ENGINE = InnoDB;
 
 INSERT INTO Users (firstname, lastname, email, password, picture, role_id)
-VALUES ('admin', 'admin', 'admin@mail.com', '$2a$10$9kuCCkLnpqz2WFt2ycj7Nux3T5PhYBLuGBznW0PNdaA9VRBqgEJgS', 'generic_profile_photo.png', 1);
+VALUES ('admin', 'admin', 'admin@mail.com', '$2a$10$9kuCCkLnpqz2WFt2ycj7Nux3T5PhYBLuGBznW0PNdaA9VRBqgEJgS', NULL, 1);
 
 -- -----------------------------------------------------
 -- Table `teddb`.`Occupation`
