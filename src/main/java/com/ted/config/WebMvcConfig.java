85083ad54a65b2ddb02f320c25c726a50a8d4a76
LSmyrnaios <lsmyrnaios@gmail.com>
2018-09-30 22:04:09 +0300

    Switch back to Json in http-responses.

--- src/main/java/com/ted/config/WebMvcConfig.java
+++ src/main/java/com/ted/config/WebMvcConfig.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(WebMvcConfig.class);
