2a50bd353c37f03d4468e05ffe305c4ee838d9c5
LSmyrnaios <lsmyrnaios@gmail.com>
2018-10-15 04:29:56 +0300

    New feature: Allow the admin to extract data from specific users! Before, the admin could only extract all of the users' data and couldn't select individual users. Note that the selection-list is kept between page-switches (by a new service), for example when the admin selects some users, then wants to go and see a user's data but then to return and extract the data from the users that previously had selected.

--- frontend/src/app/core/_services/index.ts
+++ frontend/src/app/core/_services/index.ts
@@ -1,6 +1,7 @@
 export * from './alert.service';
 export * from './authentication.service';
 export * from './user.service';
+export * from './usersCheckBox.service';
 export * from './occupation.service';
 export * from './experience.service';
 export * from './education.service';
